db7c8fd38c2e805685b8ecb5bb20b71d07ab6b01
Becky Miller <112288615+beckymiller777@users.noreply.github.com>
2022-08-27 17:49:13 -0600

    Update streamlit_app.py
    copied all code from course

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit  
 streamlit.title('My Parents New Healthy Diner')
-streamlit.title('Breakfast Menu')
+Streamlit.header(‘Breakfast Menu’)
+Streamlit.text(‘Omega 3 & Blueberry Oatmeal’)
+Streamlit.text(‘Kale,Spinach & Rocket Smoothie’)
+Streamlit.text(‘Hard-Boiled Free-Range Egg’)
+
