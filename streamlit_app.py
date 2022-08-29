765e90b1552d57460bc2d829b40d7d00eb6da03a
Becky Miller <112288615+beckymiller777@users.noreply.github.com>
2022-08-28 21:29:10 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - output it to the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
+
 import snowflake.connector
 
 
